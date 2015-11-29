4335a03d5bcb780d2036d936b23b1f92405faa26
unknown <you@example.com>
2015-11-29 01:40:07 -0500

    remove hotel functional began adding hotel description to modals

--- js/modals.js
+++ js/modals.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 
   $(".delete-hotel").on("click", function() {
-    $(this).closest('.hotel-item').remove();
-    $(this).closest('.modal').modal('hide');
-    $('.modal-backdrop').remove();
+    $(this).closest(".hotel-item").remove();
+    $(".modal-backdrop").remove();
   });
 
 });
