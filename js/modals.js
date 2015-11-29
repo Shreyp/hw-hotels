48a420f62427e48f5e04c60365ba722a5d4683ad
unknown <you@example.com>
2015-11-29 02:44:07 -0500

    added content to about modal and links to select button

--- js/modals.js
+++ js/modals.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
   $(".delete-hotel").on("click", function() {
-    $(this).closest(".hotel-item").remove();
-    $(".modal-backdrop").remove();
+    if(confirm("Are you sure?")){
+      $(this).closest(".hotel-item").remove();
+      $(".modal-backdrop").remove();
+    }
   });
 
 });
