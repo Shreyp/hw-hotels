c760e377a3e608d21799c85e0f124d05595103ed
ceckenrode <chriseckenrode@me.com>
2015-11-30 16:48:03 -0500

    fixed issue where modal freezes whole page

--- js/modals.js
+++ js/modals.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 
   $(".delete-hotel").on("click", function() {
-    if(confirm("Are you sure?")){
+      if (confirm("Are you sure?")) {
+      $(this).parent().parent().parent().hide;
+      $('body').removeClass('modal-open');
+      $('.modal-backdrop').remove();
       $(this).closest(".hotel-item").remove();
       $(".modal-backdrop").remove();
     }
